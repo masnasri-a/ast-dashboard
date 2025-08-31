fc223c30b5a12a835867504e704ac7d20391d2b1
aliendev <nasriblog12@gmail.com>
2025-08-31 22:33:59 +0700

    Add Script component for analytics tracking in RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import './globals.css';
 import { SidebarProvider } from '@/context/SidebarContext';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { Toaster } from '@/components/ui/sonner';
+import Script from 'next/script';
 
 const outfit = Outfit({
   subsets: ["latin"],
@@ -19,6 +20,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          src="https://umami.laice.tech/script.js"
+          data-website-id="9f4e8e06-e617-4269-8356-9ccc626f3a66"
+        />
+      </head>
       <body className={`${outfit.className} dark:bg-gray-900`}>
         <AuthRedirect />
         <ThemeProvider>
